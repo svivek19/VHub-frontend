fc2b9076aebe5fae3c53b70d1569aa2cec5ee5bb
svivek19 <svivek030503@gmail.com>
2026-02-15 15:39:12 +0530

    feat: implement routing with ProtectedRoute and PublicRoute, and add UI components for Avatar, Button, Input, and ScrollArea

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import AuthPage from "./pages/AuthPage";
+import ChatPage from "./pages/ChatPage";
+
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import PublicRoute from "./components/auth/PublicRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AuthPage />} />
+        <Route
+          path="/"
+          element={
+            <PublicRoute>
+              <AuthPage />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute>
+              <ChatPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
