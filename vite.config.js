59fdf71caab39282d800932c5f769445d9e1164e
svivek19 <svivek030503@gmail.com>
2026-03-14 18:27:31 +0530

    feat: implement pwa

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,37 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "MERN Chat",
+        short_name: "Chat",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#0b141a",
+        theme_color: "#0b141a",
+        icons: [
+          {
+            src: "pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
